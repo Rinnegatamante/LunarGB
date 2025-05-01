4e08b352839663144e9adc547e197eec339d8271
Rinnegatamante <rinnegatamante@gmail.com>
2025-05-01 13:25:47 +0200

    Optimize PPU code.

--- src/ppu.lua
+++ src/ppu.lua
@@ -4,26 +4,28 @@ local ppu_colors = {0xFFFFFFFF, 0xAAAAAAFF, 0x555555FF, 0x000000FF}
 ppu_dbg_tex = nil
 
 -- Indices for LCD struct
-local LCD_LCDC     = 0
-local LCD_LCDS     = 1
-local LCD_SCROLL_Y = 2
-local LCD_SCROLL_X = 3
-local LCD_LY       = 4
-local LCD_LY_CMP   = 5
-local LCD_DMA      = 6
-local LCD_BG_PAL   = 7
-local LCD_OBJ1_PAL = 8
-local LCD_OBJ2_PAL = 9
-local LCD_WIN_Y    = 10
-local LCD_WIN_X    = 11
+-- LCD_LCDC     = 0
+-- LCD_LCDS     = 1
+-- LCD_SCROLL_Y = 2
+-- LCD_SCROLL_X = 3
+-- LCD_LY       = 4
+-- LCD_LY_CMP   = 5
+-- LCD_DMA      = 6
+-- LCD_BG_PAL   = 7
+-- LCD_OBJ1_PAL = 8
+-- LCD_OBJ2_PAL = 9
+-- LCD_WIN_Y    = 10
+-- LCD_WIN_X    = 11
 
 local lcd_regs = {}
 local bg_cols = {}
 local sp1_cols = {}
 local sp2_cols = {}
 
+local bor = bit32.bor
 local band = bit32.band
 local rshift = bit32.rshift
+local lshift = bit32.lshift
 
 local function ppu_update_dbg_tile(num, x, y)
 	for tile_y = 0, 15, 2 do
@@ -31,9 +33,9 @@ local function ppu_update_dbg_tile(num, x, y)
 		local b1 = ppu_vram[offs]
 		local b2 = ppu_vram[offs + 1]
 		for bit = 7, 0, -1 do
-			local high = bit32.lshift(((bit32.band(b1, bit32.lshift(1, bit)) > 0) and 1 or 0), 1)
-			local low = (bit32.band(b2, bit32.lshift(1, bit)) > 0) and 1 or 0
-			local clr = bit32.bor(high, low) + 1
+			local high = lshift(((band(b1, lshift(1, bit)) > 0) and 1 or 0), 1)
+			local low = (band(b2, lshift(1, bit)) > 0) and 1 or 0
+			local clr = bor(high, low) + 1
 			Graphics.drawPixel(x + (7 - bit), y + (tile_y / 2), ppu_colors[clr], ppu_dbg_tex)
 		end
 	end
@@ -93,18 +95,18 @@ function ppu_vram_read(addr)
 end
 
 function lcd_init()
-	lcd_regs[LCD_LCDC] = 0x91
-	lcd_regs[LCD_LCDS] = 0x00
-	lcd_regs[LCD_SCROLL_Y] = 0
-	lcd_regs[LCD_SCROLL_X] = 0
-	lcd_regs[LCD_LY] = 0
-	lcd_regs[LCD_LY_CMP] = 0
-	lcd_regs[LCD_DMA] = 0
-	lcd_regs[LCD_BG_PAL] = 0xFC
-	lcd_regs[LCD_OBJ1_PAL] = 0xFF
-	lcd_regs[LCD_OBJ2_PAL] = 0xFF
-	lcd_regs[LCD_WIN_Y] = 0
-	lcd_regs[LCD_WIN_X] = 0
+	lcd_regs[0] = 0x91
+	lcd_regs[1] = 0x00
+	lcd_regs[2] = 0
+	lcd_regs[3] = 0
+	lcd_regs[4] = 0
+	lcd_regs[5] = 0
+	lcd_regs[6] = 0
+	lcd_regs[7] = 0xFC
+	lcd_regs[8] = 0xFF
+	lcd_regs[9] = 0xFF
+	lcd_regs[10] = 0
+	lcd_regs[11] = 0
 	
 	for i = 0, 3 do
 		bg_cols[i] = ppu_colors[i]
@@ -124,19 +126,21 @@ function lcd_write(addr, val)
 	if offs == 6 then
 		dma_start(val)
 	elseif offs == 7 then
-		bg_cols[0] = ppu_colors[band(val, 0b11)]
-		bg_cols[1] = ppu_colors[band(rshift(val, 2), 0b11)]
-		bg_cols[2] = ppu_colors[band(rshift(val, 4), 0b11)]
-		bg_cols[3] = ppu_colors[band(rshift(val, 6), 0b11)]
+		bg_cols[0] = ppu_colors[val % 4]
+		bg_cols[1] = ppu_colors[rshift(val, 2) % 4]
+		bg_cols[2] = ppu_colors[rshift(val, 4) % 4]
+		bg_cols[3] = ppu_colors[rshift(val, 6) % 4]
 	elseif offs == 8 then
-		sp1_cols[0] = ppu_colors[band(band(val, 0b11111100), 0b11)]
-		sp1_cols[1] = ppu_colors[band(rshift(band(val, 0b11111100), 2), 0b11)]
-		sp1_cols[2] = ppu_colors[band(rshift(band(val, 0b11111100), 4), 0b11)]
-		sp1_cols[3] = ppu_colors[band(rshift(band(val, 0b11111100), 6), 0b11)]
+		local v = band(val, 0b11111100)
+		sp1_cols[0] = ppu_colors[0]
+		sp1_cols[1] = ppu_colors[rshift(v, 2) % 4]
+		sp1_cols[2] = ppu_colors[rshift(v, 4) % 4]
+		sp1_cols[3] = ppu_colors[rshift(v, 6) % 4]
 	elseif offs == 9 then
-		sp2_cols[0] = ppu_colors[band(band(val, 0b11111100), 0b11)]
-		sp2_cols[1] = ppu_colors[band(rshift(band(val, 0b11111100), 2), 0b11)]
-		sp2_cols[2] = ppu_colors[band(rshift(band(val, 0b11111100), 4), 0b11)]
-		sp2_cols[3] = ppu_colors[band(rshift(band(val, 0b11111100), 6), 0b11)]
+		local v = band(val, 0b11111100)
+		sp2_cols[0] = ppu_colors[0]
+		sp2_cols[1] = ppu_colors[rshift(v, 2) % 4]
+		sp2_cols[2] = ppu_colors[rshift(v, 4) % 4]
+		sp2_cols[3] = ppu_colors[rshift(v, 6) % 4]
 	end
 end
